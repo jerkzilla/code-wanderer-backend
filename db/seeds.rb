f09377ad50aea307146a5e80c66c8f65a85adea5
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-11-11 06:52:17 -0600

    set up beginning of backend, seed, check models, routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+category = Category.create(name: 'Ruby')
+
+resource = Resource.create(category_id: 1, name: 'Tips for Newbies', img_url: 'https://res.cloudinary.com/jjmarshall-me/image/upload/v1605098638/Want-To-Become-a-Programmer-Here-Are-The-Top-5-Tips-For-Newbies_t5utnr.jpg')
+
+resource_two = Resource.create(category_id: 1, name: 'OOP', img_url: 'https://res.cloudinary.com/jjmarshall-me/image/upload/v1605098638/Object-Oriented-Programming-Concepts_hzkpt9.jpg')
